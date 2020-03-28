2cdacdbc02851c3a40df4a7e5c409592bc9da9d6
Ahmadreza Salehvand <ahmadreza.salehvand@gmail.com>
2020-03-28 13:16:52 +0430

    first commit

---
+++ src/components/Todo/Todos.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TodoItem from './TodoItem/TodoItem';
+
+const Todos = () => {
+    return (
+        <section>
+            <h2>Todo List</h2>
+            <TodoItem/>
+        </section>
+    );
+};
+
+export default Todos;
