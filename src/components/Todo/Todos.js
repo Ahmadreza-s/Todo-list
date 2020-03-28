738771f08bfc7ebc86c5fa1375c55fdeb1a5d706
Ahmadreza Salehvand <ahmadreza.salehvand@gmail.com>
2020-03-28 15:03:48 +0430

    completed TodoList

--- src/components/Todo/Todos.js
+++ src/components/Todo/Todos.js
@@ -4,10 +4,20 @@ import TodoItem from './TodoItem/TodoItem';
 const Todos = ({todos, onEditMode, onRemove, onClear}) => {
 
     return (
-        <section>
-            <h3>Todo List</h3>
-            <TodoItem/>
-        </section>
+        <ul className='list-group my-5'>
+            <h3 className='text-capitalize text-center'>Todo List</h3>
+            {
+                todos.map(todo => <TodoItem key={todo.id}
+                                            onEditMode={onEditMode}
+                                            onRemove={onRemove}
+                                            todo={todo}/>)
+            }
+            <button type='button'
+                    className='btn btn-danger btn-tn-block text-uppercase mt-5'
+                    onClick={onClear}>
+                Clear List
+            </button>
+        </ul>
     );
 };
 
