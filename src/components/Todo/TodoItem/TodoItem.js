48b7b46896d5f2afa0247202758bea5576fd9cb0
Ahmadreza Salehvand <ahmadreza.salehvand@gmail.com>
2020-03-28 15:17:21 +0430

    completed TodoItem

--- src/components/Todo/TodoItem/TodoItem.js
+++ src/components/Todo/TodoItem/TodoItem.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
-const TodoItem = () => {
-    return (
-        <div>
+const TodoItem = ({todo, onEditMode, onRemove}) => {
 
-        </div>
+    return (
+        <li className='list-group-item text-capitalize d-flex justify-content-between my-2'
+            style={{borderTopWidth: '1px'}}>
+            <h6>{todo.title}</h6>
+            <div className='todo-icon'>
+                <span className='mx-2 text-success' onClick={() => onEditMode(todo.id)}>
+                    <i className="fas fa-pencil-alt"/>
+                </span>
+                <span className='mx-2 text-danger' onClick={() => onRemove(todo.id)}>
+                    <i className="fas fa-trash-alt"/>
+                </span>
+            </div>
+        </li>
     );
 };
 
